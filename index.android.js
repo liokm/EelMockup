5462801ebaee2a5595a9b882653a0b421b9f8817
Meng Li <meng.li@eroad.com>
2016-01-13 14:51:25 +1300

    Upgrade to react-native 0.18-rc

--- index.android.js
+++ index.android.js
@@ -1,34 +1,120 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
-var React = require('react-native');
-var {
+import React from 'react-native';
+const {
   AppRegistry,
   StyleSheet,
+  Component,
+  Navigator,
   Text,
   View,
+  TouchableNativeFeedback
 } = React;
+import  { Actions, Router, Route, Schema, Animations, TabBar } from 'react-native-router-flux';
 
-var EelMockup = React.createClass({
-  render: function() {
+//var EelMockup = React.createClass({
+//  render: function() {
+//    return (
+//      <View style={styles.container}>
+//        <Text style={styles.welcome}>
+//          Welcome to React Native!
+//        </Text>
+//        <Text style={styles.instructions}>
+//          To get started, edit index.android.js
+//        </Text>
+//        <Text style={styles.instructions}>
+//          Shake or press menu button for dev menu
+//        </Text>
+//      </View>
+//    );
+//  }
+//});
+
+class Today extends Component {
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      <Text> Today </Text>
+        <TouchableNativeFeedback onPress={ Actions.launch }>
+        <View>
+        <Text>Launch</Text>
+        </View>
+        </TouchableNativeFeedback>
+      </View>
+    )
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text> Home </Text>
+        <TouchableNativeFeedback onPress={ Actions.launch }>
+        <View>
+        <Text>Launch</Text>
+        </View>
+        </TouchableNativeFeedback>
+      </View>
+    )
+  }
+}
+
+class Days extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text> 8 Days </Text>
+        <TouchableNativeFeedback onPress={ Actions.launch }>
+        <View>
+        <Text>Launch</Text>
+        </View>
+        </TouchableNativeFeedback>
+      </View>
+    )
+  }
+}
+
+class Launch extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Launch Page</Text>
+        <TouchableNativeFeedback onPress={ Actions.today }>
+        <View>
+        <Text>Today</Text>
+        </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback onPress={ Actions.days }>
+        <View>
+        <Text>8 Days</Text>
+        </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback onPress={ Actions.home }>
+        <View>
+        <Text>Home</Text>
+        </View>
+        </TouchableNativeFeedback>
       </View>
     );
   }
-});
+}
+
+class EelMockup extends Component {
+  render() {
+    return (
+       <Router hideNavBar={true}>
+         <Schema name="modal" sceneConfig={ Navigator.SceneConfigs.FloatFromBottom}/>
+         <Schema name="default" sceneConfig={ Navigator.SceneConfigs.FloatFromRight}/>
+
+         <Route name="launch" component={Launch} initial={true} title="Launch" />
+         <Route name="today" component={Today} title="Today" sceneConfig={ Navigator.SceneConfigs.FloatFromLeft } schema="modal" />
+         <Route name="days" component={Days} title="8 Days" type="replace" sceneConfig={ Navigator.SceneConfigs.FloatFromBottom } />
+         <Route name="home" component={Home} title="Home" sceneConfig={ Navigator.SceneConfigs.FadeTheRight } />
+       </Router>
+    )
+  }
+}
 
 var styles = StyleSheet.create({
   container: {
