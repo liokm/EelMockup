6a1eab68215f048f09bc3486e1c2038064c2c5e0
Meng Li <meng.li@eroad.com>
2016-01-15 13:25:18 +1300

    The lines fail the live-reload, comment them out.

--- android/app/src/main/java/com/eelmockup/MainActivity.java
+++ android/app/src/main/java/com/eelmockup/MainActivity.java
@@ -31,21 +31,22 @@ public class MainActivity extends ReactActivity {
         return BuildConfig.DEBUG;
     }
 
-   /**
-   * A list of packages used by the app. If the app uses additional views
-   * or modules besides the default ones, add more packages here.
-   */
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new OrientationPackage(this));
+        new MainReactPackage()
+        //, new OrientationPackage(this)
+      );
     }
 
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        Intent intent = new Intent("onConfigurationChanged");
-        intent.putExtra("newConfig", newConfig);
-        this.sendBroadcast(intent);
-    }
+    //public void onConfigurationChanged(Configuration newConfig) {
+    //    super.onConfigurationChanged(newConfig);
+    //    Intent intent = new Intent("onConfigurationChanged");
+    //    intent.putExtra("newConfig", newConfig);
+    //    this.sendBroadcast(intent);
+    //}
 }
